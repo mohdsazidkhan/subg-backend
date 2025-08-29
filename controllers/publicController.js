18b78d71f91ad674bcf076e5001e225b9e3f1527
tkwebdesigner <tofique.animation@gmail.com>
2025-08-29 12:50:37 +0530

    top 10 home

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -218,8 +218,8 @@ exports.getMonthlyLeaderboard = async (req, res) => {
 // Helper function to get level names
 const getLevelName = (level) => {
   const levelNames = {
-    1: 'Rookie', 2: 'Explorer', 3: 'Thinker', 4: 'Strategist', 5: 'Achiever',
-         6: 'Mastermind', 7: 'Champion', 8: 'Prodigy', 9: 'Wizard', 10: 'Legend'
+    0: 'Starter', 1: 'Rookie', 2: 'Explorer', 3: 'Thinker', 4: 'Strategist', 5: 'Achiever',
+    6: 'Mastermind', 7: 'Champion', 8: 'Prodigy', 9: 'Wizard', 10: 'Legend'
   };
   return levelNames[level] || 'Unknown';
 };
